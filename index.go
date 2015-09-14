37d783a3f9cc84431a2d2d371e53c12a7be402cb
Tianon Gravi <tianon@debian.org>
2015-09-14 00:07:19 -0700

    Add support for fetching both no-components sources (suite/) and auto-sensing mirror-supported compression based on attempting file extensions

--- index.go
+++ index.go
@@ -1,8 +1,42 @@
 package sources
 
 import (
+	"compress/bzip2"
+	"compress/gzip"
+	"fmt"
+	"net/http"
+
 	"pault.ag/go/resolver"
 )
+
+var compressions = []string{".bz2", ".gz", ""}
+
+func fetchCandidates(can *resolver.Candidates, url string) error {
+	for _, comp := range compressions {
+		resp, err := http.Get(url + comp)
+		if err != nil {
+			return err
+		}
+		if resp.StatusCode != 200 {
+			resp.Body.Close()
+			continue
+		}
+		defer resp.Body.Close()
+		switch comp {
+		case ".bz2":
+			return can.AppendBinaryIndexReader(bzip2.NewReader(resp.Body))
+		case ".gz":
+			reader, err := gzip.NewReader(resp.Body)
+			if err != nil {
+				return err
+			}
+			defer reader.Close()
+			return can.AppendBinaryIndexReader(reader)
+		}
+		return can.AppendBinaryIndexReader(resp.Body)
+	}
+	return fmt.Errorf("unable to find %s (tried all of %s)", url, compressions)
+}
 
 func (source Source) fetch(can *resolver.Candidates, arches ...string) error {
 	if len(source.Architectures) > 0 {
@@ -15,10 +49,16 @@ func (source Source) fetch(can *resolver.Candidates, arches ...string) error {
 		}
 		for _, uri := range source.URIs {
 			for _, suite := range source.Suites {
-				for _, component := range source.Components { // TODO add support for no components (suite/)
-					for _, arch := range arches {
-						if err := resolver.AppendBinaryIndex(can, uri, suite, component, arch); err != nil {
-							return err
+				if len(source.Components) == 0 && suite[len(suite)-1] == '/' {
+					if err := fetchCandidates(can, uri+"/"+suite+"Packages"); err != nil {
+						return err
+					}
+				} else {
+					for _, component := range source.Components {
+						for _, arch := range arches {
+							if err := fetchCandidates(can, fmt.Sprintf("%s/dists/%s/%s/binary-%s/Packages", uri, suite, component, arch)); err != nil {
+								return err
+							}
 						}
 					}
 				}
